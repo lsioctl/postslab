b9c9219166056cc9e0c1ba8c0c1b19c4e6443c36
lsioctl <laurent.steff@gmail.com>
2019-11-27 16:12:22 +0800

    front: styled messages

--- postslab-front/src/components/PostsList.js
+++ postslab-front/src/components/PostsList.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import './PostsList.css';
 
 function PostsList(props) {
+  // This is to scroll to the last post
+  const postsEndRef = useRef(null)
+
+  function scrollToBottom() {
+    postsEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(scrollToBottom, [props.posts]);
+
   return (
-    <div className="Home"> 
-     <ul>
-          {props.posts && props.posts.map(item => (
-            <li key={item.id}>
-              {item.user}: {item.body}
-            </li>
-          ))}
-        </ul>
+    <div className="postslist">
+      <ul className="postslist__list">
+        {props.posts && props.posts.map(item => (
+          <li className="postslist__list__item" key={item.id}>
+            <div className="postslist__list__item__message">
+              <div className="postslist__list__item__message__header">
+                {item.user}
+              </div>
+              <div className="postslist__list__item__message__content">
+                {item.body}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div ref={postsEndRef} />
     </div>
   );
 };
